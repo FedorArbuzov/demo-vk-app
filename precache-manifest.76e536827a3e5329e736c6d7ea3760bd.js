0647f8c20adb93c9cc975553b4df4db33ec6ac81
root <arbuzovfp@gmail.com>
2019-12-18 13:15:39 +0300

    Updates

---
+++ precache-manifest.76e536827a3e5329e736c6d7ea3760bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a484e3aef420ccb058155dfdb93b5202",
+    "url": "/demo-vk-app/index.html"
+  },
+  {
+    "revision": "a4aec9ff8d685a187cad",
+    "url": "/demo-vk-app/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "4469754673ce14911dc3",
+    "url": "/demo-vk-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "a4aec9ff8d685a187cad",
+    "url": "/demo-vk-app/static/js/2.a87d6afd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/demo-vk-app/static/js/2.a87d6afd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4469754673ce14911dc3",
+    "url": "/demo-vk-app/static/js/main.002c5617.chunk.js"
+  },
+  {
+    "revision": "1273fad84f7472529839",
+    "url": "/demo-vk-app/static/js/runtime-main.888b80bf.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/demo-vk-app/static/media/persik.4e1ec840.png"
+  }
+]);
